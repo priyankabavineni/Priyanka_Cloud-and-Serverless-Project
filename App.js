c0cc228b396d8bac054fcbdc55913785d031444e
priyankabavineni <121004246+priyankabavineni@users.noreply.github.com>
2024-03-29 08:36:19 +0530

    Add files via upload
    I have attached a few files; the reports help with project comprehension, and the remaining two are segments of code that can be utilised for the user interface.

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>PRIYANKA NATURE BLOG!!!</h2>
+      <video width="700px" height="400px" controls>
+        <source src="https://d2gs74vy1y65aa.cloudfront.net/190828_27_SuperTrees_HD_17.mp4" type="video/mp4"/>
+      </video>
+    </div>
+  );
+}
+
+export default App;
